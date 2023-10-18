418204386a07ee2669df1366b5bfa9bb31f7ee1d
iLauncherDev <tvistaifari@gmail.com>
2023-10-17 22:07:16 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <Arduino.h>
 #include <Adafruit_SSD1306.h>
-#define SPEAKER_PORT 0x02
+#define SPEAKER_PORT 2
+#define SPEAKER_BITS 16
+#define SPEAKER_FREQUENCY_HZ 20000000
+#define SPEAKER_FREQUENCY_KHZ (SPEAKER_FREQUENCY_HZ / 1000)
+#define SPEAKER_FREQUENCY_MHZ (SPEAKER_FREQUENCY_HZ / 1000000)
 
 Adafruit_SSD1306 display(128, 64);
 char string[64];
 
-uint32_t audio[] = {
-    1000,
-    50,
-    686,
-    327,
-    436,
-    1231,
-    (uint32_t)-1,
-};
-
-void setup()
+uint32_t speaker_read()
 {
-    display.begin(2, 0x3C);
-    pinMode(2, OUTPUT);
+    uint8_t port = SPEAKER_PORT;
+    uint32_t value = 0;
+    for (uint8_t bits = 0; bits < SPEAKER_BITS; bits += 8)
+        value |= analogRead(port++) << bits;
+    return SPEAKER_FREQUENCY_HZ * value;
 }
 
 void speaker_write(uint32_t hz)
@@ -26,31 +23,40 @@ void speaker_write(uint32_t hz)
     uint8_t port = SPEAKER_PORT;
     if (hz)
     {
-        uint32_t div = 20000000 / 1000;
-        for (uint8_t bits = 0; bits <= 8; bits += 8)
+        uint32_t div = SPEAKER_FREQUENCY_HZ / hz;
+        for (uint8_t bits = 0; bits < SPEAKER_BITS; bits += 8)
             analogWrite(port++, (div >> bits) & 0xff);
     }
     else
     {
         uint32_t div = 0;
-        for (uint8_t bits = 0; bits <= 8; bits += 8)
+        for (uint8_t bits = 0; bits < SPEAKER_BITS; bits += 8)
             analogWrite(port++, (div >> bits) & 0xff);
     }
 }
 
+void setup()
+{
+    display.begin(2, 0x3C);
+    pinMode(2, OUTPUT);
+    speaker_write(1000);
+}
+
 void loop()
 {
-    goto speaker;
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextSize(1);
     display.setTextColor(1);
     display.print("Usb Data: ");
-    display.print(itoa(analogRead(0), string, 16));
+    display.print(itoa(digitalRead(0), string, DEC));
     display.print(", ");
-    display.print(itoa(analogRead(1), string, 16));
+    display.print(itoa(digitalRead(1), string, DEC));
+    display.print(";\n");
+    display.print("Speaker Data: ");
+    display.print(itoa(speaker_read(), string, DEC));
+    display.print(", ");
+    display.print(itoa(SPEAKER_BITS, string, DEC));
     display.print(";\n");
     display.display();
-speaker:
-    speaker_write(1000);
 }
