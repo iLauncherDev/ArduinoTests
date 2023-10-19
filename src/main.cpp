9c35af8b881d6216f99bb3672e981a3c186944a1
iLauncherDev <tvistaifari@gmail.com>
2023-10-18 21:20:33 -0300

    volume

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,32 @@
 #include <Arduino.h>
 #include <Adafruit_SSD1306.h>
-#define SPEAKER_PORT 2
 #define SPEAKER_BITS 16
 #define SPEAKER_BITS_INC 8
-#define SPEAKER_FREQUENCY_HZ 25000000
+#define SPEAKER_FREQUENCY_HZ 20000000
 #define SPEAKER_FREQUENCY_KHZ (SPEAKER_FREQUENCY_HZ / 1000)
 #define SPEAKER_FREQUENCY_MHZ (SPEAKER_FREQUENCY_HZ / 1000000)
+
+uint8_t speaker_volume = 1.00f;
 
 Adafruit_SSD1306 display(128, 64);
 char string[256];
 
-uint32_t speaker_read()
+void speaker_set_volume(uint8_t percent)
 {
-    uint8_t port = SPEAKER_PORT;
+    speaker_volume = 101 - (percent & 100);
+}
+
+uint32_t speaker_read(uint8_t port)
+{
     uint32_t value = 0;
     for (uint8_t bits = 0; bits < SPEAKER_BITS; bits += SPEAKER_BITS_INC)
         value |= analogRead(port++) << bits;
     return value ? SPEAKER_FREQUENCY_HZ / value : 0;
 }
 
-void speaker_write(uint32_t hz)
+void speaker_write(uint8_t port, uint32_t hz)
 {
-    uint8_t port = SPEAKER_PORT;
-    uint32_t div = hz ? SPEAKER_FREQUENCY_HZ / hz : 0;
+    uint32_t div = hz ? SPEAKER_FREQUENCY_HZ / (int16_t)round((float)hz * speaker_volume) : 0;
     for (uint8_t bits = 0; bits < SPEAKER_BITS; bits += SPEAKER_BITS_INC)
         analogWrite(port++, (div >> bits) & 0xff);
 }
@@ -30,11 +34,13 @@ void speaker_write(uint32_t hz)
 void setup()
 {
     display.begin(2, 0x3C);
-    speaker_write(1000);
+    speaker_set_volume(100);
 }
 
 void loop()
 {
+    speaker_write(2, 1000);
+    speaker_write(4, 1000);
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextSize(1);
@@ -45,7 +51,9 @@ void loop()
     display.print(itoa(digitalRead(1), string, DEC));
     display.print(";\n");
     display.print("Speaker Data: ");
-    display.print(itoa(speaker_read(), string, DEC));
+    display.print(itoa(speaker_read(2), string, DEC));
+    display.print(", ");
+    display.print(itoa(speaker_read(4), string, DEC));
     display.print(", ");
     display.print(itoa(SPEAKER_BITS, string, DEC));
     display.print(";\n");
