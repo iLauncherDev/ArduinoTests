a8a313f7f9cd24c0e174c178a8bbc473e6a98dd3
Daniel Victor <tvistaifari@gmail.com>
2024-02-03 20:13:37 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,21 @@
 
 uint8_t a = 12;
 
+void IRAM_ATTR blink()
+{
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
 void setup()
 {
-    tone(a, 20 * 1000, 0);
+    pinMode(LED_BUILTIN, OUTPUT);
+    hw_timer_t *blink_timer = timerBegin(1, 80, true);
+    timerAttachInterrupt(blink_timer, blink, true);
+    timerAlarmWrite(blink_timer, 1000000, true);
+    timerAlarmEnable(blink_timer);
 }
 
 void loop()
 {
+    tone(a, rand() % (15 * 1000), 0);
 }
