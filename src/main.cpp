576261f19e89a99838b4f832a806adc16c0a844a
Daniel Victor <tvistaifari@gmail.com>
2024-02-11 16:20:49 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
 #include <Arduino.h>
 
-uint8_t a = 12;
+const int r = 26, g = 25, b = 17, hsync = 16, vsync = 27;
 
-void IRAM_ATTR blink()
+void IRAM_ATTR vsyncINT()
 {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    digitalWrite(vsync, HIGH);
+    digitalWrite(vsync, LOW);
+}
+
+void IRAM_ATTR hsyncINT()
+{
+    digitalWrite(hsync, HIGH);
+    digitalWrite(hsync, LOW);
 }
 
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
-    hw_timer_t *blink_timer = timerBegin(1, 80, true);
-    timerAttachInterrupt(blink_timer, blink, true);
-    timerAlarmWrite(blink_timer, 1000000, true);
-    timerAlarmEnable(blink_timer);
-    tone(a, 18 * 1000, 0);
+    pinMode(r, OUTPUT);
+    pinMode(g, OUTPUT);
+    pinMode(b, OUTPUT);
+    pinMode(hsync, OUTPUT);
+    pinMode(vsync, OUTPUT);
+    hw_timer_t *vsyncINT_timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(vsyncINT_timer, vsyncINT, true);
+    timerAlarmWrite(vsyncINT_timer, 16 * 1000, true);
+    timerAlarmEnable(vsyncINT_timer);
+    hw_timer_t *hsyncINT_timer = timerBegin(1, 80, true);
+    timerAttachInterrupt(hsyncINT_timer, hsyncINT, true);
+    timerAlarmWrite(hsyncINT_timer, 32, true);
+    timerAlarmEnable(hsyncINT_timer);
 }
 
 void loop()
